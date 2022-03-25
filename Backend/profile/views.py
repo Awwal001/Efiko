fd847b69b12caae800339bf4ab6a726a2a94d345
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-03-25 11:02:22 +0100

    Add files via upload

---
+++ Backend/profile/views.py
@@ -0,0 +1,42 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from rest_framework import permissions
+
+class GetUserProfileView(APIView):
+    permission_classes = [permissions.AllowAny]
+    def get(self, request, format=None):
+        try:
+            user = self.request.user
+            username = user.username
+
+            user_profile = UserProfile.objects.get(user=user)
+            user_profile = UserProfileSerializer(user_profile)
+
+            return Response({ 'profile': user_profile.data, 'username': str(username) })
+        except:
+            return Response({ 'error': 'Something went wrong when retrieving profile' })
+
+class UpdateUserProfileView(APIView):
+    permission_classes = [permissions.AllowAny]
+    def put(self, request, format=None):
+        try:
+            user = self.request.user
+            username = user.username
+
+            data = self.request.data
+            first_name = data['first_name']
+            last_name = data['last_name']
+            phone = data['phone']
+            city = data['city']
+            
+
+            UserProfile.objects.filter(user=user).update(first_name=first_name, last_name=last_name, phone=phone, city=city)
+
+            user_profile = UserProfile.objects.get(user=user)
+            user_profile = UserProfileSerializer(user_profile)
+
+            return Response({ 'profile': user_profile.data, 'username': str(username) })
+        except:
+            return Response({ 'error': 'Something went wrong when updating profile' })
